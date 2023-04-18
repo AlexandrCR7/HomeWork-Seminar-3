3d9728b00c79c4281d5ea6a812063ecc618d1b6b
Alexandr <k9108@mail.ru>
2023-04-18 17:55:18 +0300

    add new home work

---
+++ HWTask21v2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int x1 = Print("Введите х1: ");
+int x2 = Print("Введите х2: ");
+int y1 = Print("Введите y1: ");
+int y2 = Print("Введите y2: ");
+int z1 = Print("Введите z1: ");
+int z2 = Print("Введите z2: ");
+
+int Print(string arg1)
+{
+    System.Console.Write(arg1);
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("its not a number");
+    }
+    return i;
+}
+
+double Third(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double number = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+    return number;
+}
+
+System.Console.WriteLine("Расстояние между точками = " + Third(x1, x2, y1, y2, z1, z2));
